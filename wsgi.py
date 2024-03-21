9d3ae4d0f11c5ebef738992a936f716bd4aae21e
mfonPeeter <godimfon@gmail.com>
2024-03-21 10:12:33 +0100

    add flask_cors to handle cors

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from dotenv import load_dotenv
 from views.blog import blog_blueprint
 import os
 
 app = Flask(__name__)
+CORS(app, origins=['http://localhost:5173'])
 
 app.register_blueprint(blog_blueprint, url_prefix='/blog')
 
